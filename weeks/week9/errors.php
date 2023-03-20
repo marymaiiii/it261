c5f96494922de35b82f19af152b3933574cd5991
Mai Nguyen <122258917+marymaiiii@users.noreply.github.com>
2023-03-20 15:29:06 -0700

    Add files via upload

---
+++ weeks/week9/errors.php
@@ -0,0 +1,15 @@
+<?php
+//errors page which we will be using a for each loop
+// we are going to be counting errors
+// if we have more than ZERO errors, not a good thing, we will need to echo our message
+
+if(count($errors) > 0 ) :?>
+<div class="error">
+<?php foreach($errors as $error) : ?>
+    <p>
+        <?= $error ?>
+    </p>
+<?php endforeach;?>
+</div>
+<!-- end div error-->
+<?php endif ;?>
---
+++ weeks/week9/index.php
@@ -0,0 +1,61 @@
+<?php
+
+
+// our index page which must show the session_start()
+session_start();
+
+include('config.php');
+
+if(!isset($_SESSION['username'])) {
+    $_SESSION['msg'] = 'You must login first';
+    header('Location:login.php');
+
+}
+if(isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header('Location:login.php');
+
+}
+include('.includes/header.php');
+?>
+
+<header>
+
+
+<?php
+//logic - is our session success set?
+if(isset($_SESSION['success']))   ;?>
+<div class="success">
+<h3> Welcome, 
+<?php echo $_SESSION['success'];
+unset($_SESSION['success']); ?>
+</h3>
+</div>
+<!-- close success div-->
+<?php endif; ?>
+
+<?php //is our wonderful session username set????
+
+if(isset($_SESSION['username'])) ;?>
+<div class="welcome-logout">
+<h3>
+<?= $_SESSION['username'] ?>
+</h3>
+<p><a href= "index.php?logout='1'">Logout</a></p>
+
+
+</div>
+<!-- close welcome logout-->
+</header>
+
+<?php endif; ?>
+</header>
+<div id="wrapper">
+<h1> Welcome to our homepage!!!<h1>                                                                                                                      
+
+</div>
+
+<?php 
+include('./includes/footer.php');
+
